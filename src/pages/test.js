a7677ee300ca119667f2c52fba1555edf433d365
cagdaskarabulut <cagdas.karabulut@gmail.com>
2024-01-22 23:34:24 +0300

    .

---
+++ src/pages/test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Component from '../components/login-btn';
+const test = () => {
+  return (
+    <div>test
+      <Component/>
+    </div>
+  )
+}
+
+export default test
